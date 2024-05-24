b2bbb8ad169722c89d9544927e3fc7f35856a336
Kartikeya Mishra <katt1bh4@gmail.com>
2024-05-25 00:14:27 +0530

    adds the chained calculationfunctionaltiy introduces chained calculation minor ui changes

--- script.js
+++ script.js
@@ -1,106 +1,29 @@
 const display=document.querySelector('.display');
 
 
-console.log(display.textContent);
-
-function add(a,b){
-    return a+b;
-}
-function subtract(a,b){
-    return a-b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return a/b;
-}
-function modulo(a,b){
-    return a%b;
-}
-
-let num1=0;
-let num2=0;
+let previousOperand='';
+let currentOperand='';
 let op='';
-let runningTotal=0;
+let total ='';
 
 function show(str){
     display.textContent=str;
 }
 
-
-const zero = document.querySelector('.zero');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
 const equals =document.querySelector('.equals');
 const ac = document.querySelector('.ac');
 const clear = document.querySelector('.clr');
 
-seven.addEventListener('click',()=>{
-    console.log(seven.value);
-    let val = display.textContent.concat(seven.value);
-    show(val);
-})
+numarr = [...document.querySelectorAll('.number')];
 
-eight.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(eight.value);
+numarr.forEach(number=>{
+    number.addEventListener('click',()=>{
+        if(total!=''){
+            show('');
+        }
+        let val = display.textContent.concat(number.value);
     show(val);
-})
-
-nine.addEventListener('click',()=>{
-    console.log(nine.value);
-    let val = display.textContent.concat(nine.value);
-    show(val);
-})
-
-four.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(four.value);
-    show(val);
-})
-
-five.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(five.value);
-    show(val);
-})
-
-six.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(six.value);
-    show(val);
-})
-
-one.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(one.value);
-    show(val);
-})
-
-two.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(two.value);
-    show(val);
-})
-
-three.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(three.value);
-    show(val);
-})
-
-zero.addEventListener('click',()=>{
-    console.log(eight.value);
-    let val = display.textContent.concat(zero.value);
-    show(val);
+    })
 })
 
 oparr = [...document.querySelectorAll('.operator')];
@@ -108,36 +31,47 @@ oparr = [...document.querySelectorAll('.operator')];
 oparr.forEach(element => {
     element.addEventListener('click',()=>{
         let content = parseFloat(display.textContent);
-        op = element.value;
-        console.log(content);
-        if(content!=NaN){
-            num1=content;
-        }else{
-            display.textContent='INVALID INPUT'
+        if(content==''){
+            return;
         }
-        display.textContent='';
+        else if(currentOperand==''){
+            currentOperand=content;
+            op=element.value;
+            console.log(currentOperand);
+            console.log(op);
+            show('');
+        }
+        else if(currentOperand!=''){
+            if(typeof content !=="number"){
+                return;
+            }
+            previousOperand=currentOperand;
+            currentOperand=content;
+            total = operate(previousOperand,currentOperand,op);
+            show(total);
+            currentOperand=total;
+            op=element.value;
+        }
     });    
 });
 
+
 equals.addEventListener('click',()=>{
-    let content = parseInt(display.textContent);
-    num2 = content;
-    console.log(num1+' '+num2);
-    let result = operate(num1,num2,op);
-    if(result!=undefined){
-        display.textContent=result;
-    }else{
-        display.textContent='Error!';
-    }
-    runningTotal=0;
-    console.log(result);
+    previousOperand=currentOperand;
+    currentOperand=display.textContent;
+    total = operate(previousOperand,currentOperand,op);
+    show(total);
+    previousOperand='';
+    currentOperand='';
+    total='';
+    op='';
 })
 
 ac.addEventListener('click',()=>{
     display.textContent='';
-    num1=0;
-    num2=0;
-    runningTotal=0;
+    previousOperand='';
+    currentOperand='';
+    total='';
     op='';
 })
 
@@ -147,11 +81,39 @@ clear.addEventListener('click',()=>{
 })
 
 function operate(num1,num2,operator){
-    if(operator=='+') return add(num1,num2);
-    else if(operator=='-') return subtract(num1,num2);
-    else if(operator=='x') return multiply(num1,num2);
-    else if(operator=='/') return divide(num1,num2);
-    else if(operator=='%') return modulo(num1,num2);
+    console.log(num1)
+    console.log(num2)
+    let final;
+    if(operator=='+'){
+        final = parseFloat(num1) + parseFloat(num2);
+        //final=Math.round((final + Number.EPSILON) * 100) / 100;
+        return (final);
+    }else if(operator=='-'){
+        final = num1-num2;
+        return (final);
+    }else if(operator=='x'){
+        final = num1*num2;
+        return (final);
+    }else if(operator=='/'){
+        if(num2==0){
+            show('DIVISION BY ZERO!!');
+            return;
+        }else{
+            final = num1/num2;
+            final = final.toFixed(5);
+            //final=Math.round((final + Number.EPSILON) * 100) / 100;
+            return (final);
+        }
+    }else if(operator=='%'){
+        if(num2==0){
+            show('DIVISION BY ZERO!!');
+            return;
+        }else{
+            final = num1%num2;
+            final=Math.round((final + Number.EPSILON) * 100) / 100;
+            return (final);
+        }
+    }
 }
 
 
